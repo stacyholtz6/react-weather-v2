e27233ed6726e19e802bdcd3bdb9fb35520ea811
Stacy Holtz <stacyholtz6@gmail.com>
2020-05-04 19:28:33 -0600

    added state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import SearchBar from './components/SearchBar';
+import DayCard from './components/DayCard';
+import DayDetails from './components/DayDetails';
+import sampleData from './data/sample.json';
 
+console.log(sampleData);
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    days: [],
+    location: '',
+    selectedDay: null,
+    searchTerm: '',
+  });
+
+  const { days, location, selectedDay, searchTerm } = data;
+
   return (
-    <div className="App">
-     It works......
-    </div>
+    <Container>
+      <Row>
+        <Col md={7}>
+          <h1>Weather for City, ST</h1>
+        </Col>
+        <Col md={5}>
+          <SearchBar />
+        </Col>
+      </Row>
+      <Row>
+        {sampleData.data.map((day) => (
+          <DayCard />
+        ))}
+      </Row>
+      <Row>
+        <Col>
+          <DayDetails />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Container, Col, Row } from 'reactstrap';
 
 const DayCard = () => {
-  return <p>DayCard</p>;
+  return (
+    <Container>
+      <p>Day Card</p>
+    </Container>
+  )
 };
 
 export default DayCard;
